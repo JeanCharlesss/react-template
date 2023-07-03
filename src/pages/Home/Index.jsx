ca1ef6ad7dcf6320092d216289ba2c9b3665922c
Jean Charles <jean.fullstack01@gmail.com>
2023-07-03 19:12:05 -0300

    initial

---
+++ src/pages/Home/Index.jsx
@@ -0,0 +1,11 @@
+
+export default function Index() {
+  return (
+    <>
+      <h4>My React Template</h4>
+      <a target="_blank" href="https://www.github.com/JeanCharlesss/react-template">Github</a>
+    </>
+  )
+}
+
+
